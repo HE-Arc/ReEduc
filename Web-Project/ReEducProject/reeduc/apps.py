c7c52fc4af640b67dea0a61a186359fa6579610d
Schaffora <schaffora@gmail.com>
2017-03-06 12:49:57 +0100

    Functionnal migration

---
+++ Web-Project/ReEducProject/reeduc/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReeducConfig(AppConfig):
+    name = 'reeduc'
---
+++ Web-Project/ReEducProject/reeduc/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+
+# Create your models here.
+class Game(models.Model):
+    name=models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    size = models.IntegerField()
+    weight = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    strong_arm = models.CharField(max_length=1)
+    handicap = models.CharField(max_length=1000)
+    birthdate = models.DateField()
+    spec_id = models.CharField(max_length=50)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], blank=True,max_length=15)
+
+class PlayedGame(models.Model):
+    game= models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    comments = models.CharField(max_length=1000)
+    needed_time = models.IntegerField()
+    score = models.IntegerField()
+    errors = models.IntegerField()
+
