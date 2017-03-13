2b9de7a704e34e09bca78733daadc6d0c4b5de23
cyrilmanuel <cyriljeanneret@gmail.com>
2017-03-13 15:59:34 +0100

    modification du template de base de l application

--- Web-Project/ReEducProject/reeduc/urls.py
+++ Web-Project/ReEducProject/reeduc/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name="index"),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), {'connect': True}, name="index"),
     url(r'^home', TemplateView.as_view(template_name='home.html'), name="home"),
     url(r'^login', TemplateView.as_view(template_name='login.html'), name="login"),
     url(r'^subscribe', TemplateView.as_view(template_name='subscribe.html'), name="subscribe"),
