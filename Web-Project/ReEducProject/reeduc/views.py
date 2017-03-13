45ce7593234964d36a76403154ef45016d969751
cyrilmanuel <cyriljeanneret@gmail.com>
2017-03-13 16:10:45 +0100

    verrification du passage de parametre pour simuler un utilisateur connecter ou non. permet de mieux visualiser les changement d'affichage.

--- Web-Project/ReEducProject/reeduc/views.py
+++ Web-Project/ReEducProject/reeduc/views.py
@@ -3,18 +3,19 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+connection = False
 
 def index(request):
-    return render(request, 'index.html', {'connection': True})
+    return render(request, 'index.html', {'connection': connection})
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'connection': connection})
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'connection': connection})
 
 def subscribe(request):
-    return render(request, 'subscribe.html')
+    return render(request, 'subscribe.html', {'connection': connection})
 
 def view(request):
-    return render(request, 'view.html')
+    return render(request, 'view.html', {'connection': connection})
