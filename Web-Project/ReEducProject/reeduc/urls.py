e63ffd0217a275f41c389a1abddc507982adcd14
Schaffora <schaffora@gmail.com>
2017-03-13 14:01:25 +0100

    Routes added

--- Web-Project/ReEducProject/reeduc/urls.py
+++ Web-Project/ReEducProject/reeduc/urls.py
@@ -3,4 +3,8 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name="index"),
+    url(r'^home', TemplateView.as_view(template_name='home.html'), name="home"),
+    url(r'^login', TemplateView.as_view(template_name='login.html'), name="login"),
+    url(r'^subscribe', TemplateView.as_view(template_name='subscribe.html'), name="subscribe"),
+    url(r'^view', TemplateView.as_view(template_name='view.html'), name="view"),
 ]
