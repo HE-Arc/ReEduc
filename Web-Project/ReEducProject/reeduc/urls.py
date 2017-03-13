b577123a41e7cd3f1b78b4c3d02d7cfb12cea6ed
cyrilmanuel <cyriljeanneret@gmail.com>
2017-03-13 13:16:45 +0100

    ajout d'une route avec un fichier html contenant materialize

--- Web-Project/ReEducProject/reeduc/urls.py
+++ Web-Project/ReEducProject/reeduc/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from . import views
+from django.views.generic.base import TemplateView
+
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="index"),
 ]
--- Web-Project/ReEducProject/reeduc/views.py
+++ Web-Project/ReEducProject/reeduc/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    return HttpResponse("<p>It works</p>")
+
+#from django.shortcuts import render
+
+#def index(request):
+    #return render(request, '<p>dsl</p>')
