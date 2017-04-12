1a40a6a2ed722a4b338011517bf3b33178a2cc4a
cyrilmanuel <cyriljeanneret@gmail.com>
2017-04-12 15:32:43 +0200

    modification du fichier settings.py pour rajouter la redirection du décorateur LOGIN_URL

--- Web-Project/ReEducProject/reeduc/views.py
+++ Web-Project/ReEducProject/reeduc/views.py
@@ -1,5 +1,5 @@
 from chartit import DataPool, Chart
-from reeduc.models import PlayedGame, Player
+from .models import PlayedGame, Player
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
@@ -9,10 +9,11 @@ from django.db.models import Max
 from django.db.models import Min
 
 
-#Website authentification pages
+# Website authentification pages
 def logout(request):
     auth_logout(request)
     return render(request, 'login.html')
+
 
 def login(request):
     username = request.POST.get('username')
@@ -24,9 +25,9 @@ def login(request):
     else:
         return render(request, 'login.html')
 
-def subscribe(request):
 
-    #Récupération des infos du formulaires
+def subscribe(request):
+    # Récupération des infos du formulaires
     user_username = request.POST.get('username')
     user_password = request.POST.get('password')
     user_email = request.POST.get('email')
@@ -39,25 +40,25 @@ def subscribe(request):
     player_height = request.POST.get('height')
     player_weight = request.POST.get('weight')
 
-    #Fingers
+    # Fingers
     player_thumbleft = request.POST.get('thumbleft', False)
     player_forefingerleft = request.POST.get('forefingerleft', False)
     player_middlefingerleft = request.POST.get('middlefingerleft', False)
     player_ringfingerleft = request.POST.get('ringfingerleft', False)
     player_littlefingerleft = request.POST.get('littlefingerleft', False)
 
-    player_thumbright = request.POST.get('thumbright',False)
-    player_forefingerright = request.POST.get('forefingerright',False)
-    player_middlefingerright = request.POST.get('middlefingerright',False)
-    player_ringfingerright = request.POST.get('ringfingerright',False)
+    player_thumbright = request.POST.get('thumbright', False)
+    player_forefingerright = request.POST.get('forefingerright', False)
+    player_middlefingerright = request.POST.get('middlefingerright', False)
+    player_ringfingerright = request.POST.get('ringfingerright', False)
     player_littlefingerright = request.POST.get('littlefingerright', False)
 
-    #Arms
+    # Arms
     player_leftarm = request.POST.get('leftarm', False)
     player_rightarm = request.POST.get('rightarm', False)
-    player_strongarm=request.POST.get('player_strongarm')
+    player_strongarm = request.POST.get('player_strongarm')
 
-    if user_username is not None: # TODO Devra être remplacé par une validation de formulaire
+    if user_username is not None:  # TODO Devra être remplacé par une validation de formulaire
         if not User.objects.filter(username=user_username).exists():
             if not User.objects.filter(email=user_email).exists():
 
@@ -75,17 +76,17 @@ def subscribe(request):
                 HandicapsVals.append(player_leftarm)
                 HandicapsVals.append(player_rightarm)
 
-                strHandicaps =""
+                strHandicaps = ""
                 for i in range(len(HandicapsVals)):
                     if HandicapsVals[i]:
                         strHandicaps += "F;"
                     else:
                         strHandicaps += "T;"
-                genderValue=''
-                if player_gender=='Man':
-                    genderValue='M'
+                genderValue = ''
+                if player_gender == 'Man':
+                    genderValue = 'M'
                 else:
-                    genderValue='F'
+                    genderValue = 'F'
 
                 strongArmValue = ''
                 if player_strongarm == 'Left':
@@ -105,10 +106,10 @@ def subscribe(request):
                 user.set_password(user_password)
                 user.save()
 
-                #Generate spec_id
+                # Generate spec_id
                 player_spec_id = get_random_string(length=6)
-                while(Player.objects.filter(spec_id=player_spec_id).exists()):
-                    player_spec_id=get_random_string(length=6)
+                while (Player.objects.filter(spec_id=player_spec_id).exists()):
+                    player_spec_id = get_random_string(length=6)
 
                 # Create a new Player (player is an extention of user)
                 p = Player(user=User.objects.get(username=user_username),
@@ -128,23 +129,23 @@ def subscribe(request):
         return render(request, 'subscribe.html')
 
 
-
-#Website content pages
+# Website content pages
 def index(request):
     return render(request, 'index.html')
 
 
-@login_required(login_url='/login/')
+@login_required
 def home(request):
     return render(request, 'home.html')
 
+
 # Best decorator ever ->
-@login_required(login_url='/login/')
+@login_required
 def view(request):
     # Get actual connected user and get corresponding player datas
-    actualPlayer=Player.objects.get(user_id=request.user.id)
+    actualPlayer = Player.objects.get(user_id=request.user.id)
     # Get all played games by this user
-    playedGames=PlayedGame.objects.filter(player_id=actualPlayer, game_id=1)
+    playedGames = PlayedGame.objects.filter(player_id=actualPlayer, game_id=1)
     # Step 1: Create a DataPool with the data we want to retrieve.
     datas = \
         DataPool(
@@ -177,24 +178,26 @@ def view(request):
                     'text': 'Date'}}})
 
     # Step 3: Send the chart object to the template.
-    return render(request, 'view.html', {'chart': cht,'playedGames':playedGames})
+    return render(request, 'view.html', {'chart': cht, 'playedGames': playedGames})
 
-@login_required(login_url='/login/')
+
+@login_required
 def homeSelected(request):
     return render(request, 'homeSelected.html')
 
-@login_required(login_url='/login/')
+
+@login_required
 def account(request):
-    actual_user= User.objects.get(pk=request.user.id)
+    actual_user = User.objects.get(pk=request.user.id)
     actual_player = Player.objects.get(user_id=request.user.id)
     played_games = PlayedGame.objects.filter(player_id=actual_player.pk)
-    best_score=PlayedGame.objects.filter(player_id=actual_player.pk).aggregate(Max('score'))
-    max_score=best_score.get('score__max')
+    best_score = PlayedGame.objects.filter(player_id=actual_player.pk).aggregate(Max('score'))
+    max_score = best_score.get('score__max')
     best_time = PlayedGame.objects.filter(player_id=actual_player.pk).aggregate(Min('needed_time'))
-    min_time=best_time.get('needed_time__min')
+    min_time = best_time.get('needed_time__min')
     return render(request, 'account.html', {'actual_user': actual_user,
-                                            'actual_player':actual_player,
-                                            'played_games':played_games,
+                                            'actual_player': actual_player,
+                                            'played_games': played_games,
                                             'max_score': max_score,
-                                            'min_time':min_time})
-# TODO Récupérer la valeur du jeu sélectionner dans home et remplace le game_id=1 dans la fonction view ci-dessus pas le bon id
+                                            'min_time': min_time})
+    # TODO Récupérer la valeur du jeu sélectionner dans home et remplace le game_id=1 dans la fonction view ci-dessus pas le bon id
