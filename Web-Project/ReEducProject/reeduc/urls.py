dbc73c13610b16f18e6d2fae0dd9a267a34707f3
cyrilmanuel <cyriljeanneret@gmail.com>
2017-04-13 18:23:06 +0200

    ajout page apres game mais salle parce que la variable n est pas issue de la bdd pour les jeux

--- Web-Project/ReEducProject/reeduc/urls.py
+++ Web-Project/ReEducProject/reeduc/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^home', views.home, name="home"),
+    url(r'^home/$', views.home, name="home"),
+    url(r'^home/([0-9]*)/$', views.view, name="view"),
     url(r'^login', views.login, name="login"),
     url(r'^logout', views.logout, name="logout"),
     url(r'^subscribe', views.subscribe, name="subscribe"),
     url(r'^account', views.account, name="account"),
-    url(r'^view', views.view, name="view"),
+
 ]
 
